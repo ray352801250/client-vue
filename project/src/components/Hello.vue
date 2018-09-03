808a7b5c3b7c2f888520da2055449aafa2f1e69d
tianguanghui <tianguanghui@dodoca.com>
2018-09-03 17:54:34 +0800

    vue-前端

---
+++ project/src/components/Hello.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<h1>hello ..........{{aaa}}</h1>
+		<button @click="getData">FA</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'hello',
+		data() {
+			return {
+				aaa : {}
+			}
+		},
+		methods : {
+			init : function(){
+				var _self = this;
+				_self.aaa = "tyest2";
+			},
+			
+			getData : function(){
+				console.log("进入方法");
+				var _self = this;
+				  $.ajax({
+                    type : "GET",
+                    url : "http://localhost:8080/hello",
+                    dataType : "json",
+                    ContentType : "application/json",
+                    success : function(data) {
+                    	console.log("data " + data);
+　　　　　　　　　　　　　_self.aaa = data;
+                    },
+                    error : function(){
+                        alert("错误");
+                    }
+                });
+			}
+		},
+		mounted : function(){
+			this.init();
+		}
+		
+		
+	}
+</script>
+
+<style>
+</style>
---
+++ project/test/unit/feature.js
@@ -0,0 +1,163 @@
+{
+	"bgcolor": "",
+	"data": [{
+		"type": "banner",
+		"content": {
+			"all_goods": 3021,
+			"guider_style": "user",
+			"banner_img": "2018/08/04/FlbcFBwREUFrpiyPwtIVeMhSQ1MP.jpg",
+			"active_color": "#006699",
+			"new_goods": 1000,
+			"style": "2"
+		}
+	}, {
+		"type": "nav",
+		"content": {
+			"item_list": [{
+				"link_type": "feature",
+				"img": "image/tag-icon.png",
+				"data": {
+					"feature_id": 10350560
+				},
+				"link_url": "https://shop13329835.wxrrd.com/feature/10350560",
+				"link_title": "微页面|产品分类3",
+				"title": "产品分类"
+			}, {
+				"link_type": "feature",
+				"bg_color": "#ffffff",
+				"img": "image/tag-icon.png",
+				"data": {
+					"feature_id": 10350785
+				},
+				"font_color": "#000000",
+				"link_url": "https://shop13329835.wxrrd.com/feature/10350785",
+				"link_title": "微页面|赛鸽医院",
+				"title": "赛鸽门诊"
+			}, {
+				"link_type": "feature",
+				"img": "image/tag-icon.png",
+				"data": {
+					"feature_id": 10350786
+				},
+				"link_url": "https://shop13329835.wxrrd.com/feature/10350786",
+				"link_title": "微页面|铭鸽展卖",
+				"title": "铭鸽展卖"
+			}, {
+				"link_type": "feature",
+				"img": "image/tag-icon.png",
+				"data": {
+					"feature_id": 10350787
+				},
+				"link_url": "https://shop13329835.wxrrd.com/feature/10350787",
+				"link_title": "微页面|名师讲堂",
+				"title": "名师讲堂"
+			}, {
+				"link_type": "custom",
+				"img": "image/tag-icon.png",
+				"data": {
+					"feature_id": 10350560
+				},
+				"link_url": "https://mp.weixin.qq.com/s/s9NG0kDTf7wC7sReKSmHJg",
+				"link_title": "自定义外链",
+				"title": "关于我们"
+			}],
+			"icon_size": 34,
+			"style": "nav"
+		}
+	}, {
+		"type": "search",
+		"content": {
+			"bg_color": "#007cba",
+			"active_color": "#006699",
+			"style": "1"
+		}
+	}, {
+		"type": "image",
+		"content": {
+			"border_width": "4",
+			"tableContent": "<tr><td style=\"border-width:4px\" class=\"empty\" data-x=\"0\" data-y=\"0\"></td><td style=\"border-width:4px\" class=\"not-empty cols-2 rows-1\" colspan=\"2\" rowspan=\"1\"><a href=\"https://shop13329835.wxrrd.com/goods/116720859\"><img width=\"100%\" src=\"{url_ms}2018/08/27/FlCI0lPpfq9N4_aLTbDTxdNoXWRc.png\"></a></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"3\" data-y=\"0\"></td></tr><tr><td style=\"border-width:4px\" class=\"not-empty cols-2 rows-1\" colspan=\"2\" rowspan=\"1\"><a href=\"https://shop13329835.wxrrd.com/goods/116720853\"><img width=\"100%\" src=\"{url_ms}2018/08/27/FmN6ATzJfKTgtBVFVJQpz7I0ysRj.png\"></a></td><td style=\"border-width:4px\" class=\"not-empty cols-2 rows-1\" colspan=\"2\" rowspan=\"1\"><a href=\"https://shop13329835.wxrrd.com/goods/116720857\"><img width=\"100%\" src=\"{url_ms}2018/08/27/FnnWl-f8UKC0y-sXxPh2LxsNah-K.png\"></a></td></tr><tr><td style=\"border-width:4px\" class=\"empty\" data-x=\"0\" data-y=\"2\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"1\" data-y=\"2\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"2\" data-y=\"2\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"3\" data-y=\"2\"></td></tr><tr><td style=\"border-width:4px\" class=\"empty\" data-x=\"0\" data-y=\"3\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"1\" data-y=\"3\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"2\" data-y=\"3\"></td><td style=\"border-width:4px\" class=\"empty\" data-x=\"3\" data-y=\"3\"></td></tr>",
+			"item_list": [{
+				"link_type": "goods",
+				"data": {
+					"goods_id": "116720859"
+				},
+				"image_url": "2018/08/27/FlCI0lPpfq9N4_aLTbDTxdNoXWRc.png",
+				"x": 1,
+				"width": 2,
+				"link_url": "https://shop13329835.wxrrd.com/goods/116720859",
+				"y": 0,
+				"index": 0,
+				"link_title": "商品|钻石卡",
+				"type": "cube_selection",
+				"title": "",
+				"height": 1
+			}, {
+				"link_type": "goods",
+				"data": {
+					"goods_id": "116720853"
+				},
+				"image_url": "2018/08/27/FmN6ATzJfKTgtBVFVJQpz7I0ysRj.png",
+				"x": 0,
+				"width": 2,
+				"link_url": "https://shop13329835.wxrrd.com/goods/116720853",
+				"y": 1,
+				"index": 1,
+				"link_title": "商品|金卡",
+				"type": "cube_selection",
+				"title": "",
+				"height": 1
+			}, {
+				"link_type": "goods",
+				"data": {
+					"goods_id": "116720857"
+				},
+				"image_url": "2018/08/27/FnnWl-f8UKC0y-sXxPh2LxsNah-K.png",
+				"x": 2,
+				"width": 2,
+				"link_url": "https://shop13329835.wxrrd.com/goods/116720857",
+				"y": 1,
+				"index": 2,
+				"link_title": "商品|银卡",
+				"type": "cube_selection",
+				"title": "",
+				"height": 1
+			}]
+		}
+	}, {
+		"type": "goods_tag",
+		"content": {
+			"show_csale": false,
+			"valid": true,
+			"number": "18",
+			"selected_tag": [{
+				"shop_id": 13329835,
+				"updated_at": "2018-07-09 17:47:18",
+				"name": "店长推荐",
+				"description": "",
+				"created_at": "2018-07-09 17:47:18",
+				"link_url": "https://shop13329835.wxrrd.com/tag/841915",
+				"id": 841915,
+				"merchant_id": 10042768,
+				"type": "0",
+				"content": ""
+			}],
+			"size": "small",
+			"show_title": true,
+			"show_cart": true,
+			"show_price": true,
+			"sort": "created_at"
+		}
+	}, {
+		"type": "phone",
+		"content": {
+			"phone": "029-81101397",
+			"name": "联系电话"
+		}
+	}],
+	"description": "",
+	"id": 10348001,
+	"top_pendant": 1,
+	"title": "天傲赛鸽官方商城",
+	"type": 0,
+	"side_pendant": 1
+}
